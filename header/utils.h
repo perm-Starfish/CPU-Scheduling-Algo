88c65c45c7e383217ced5b02186025ee1043c56d
Starfish <82247390+perm-Starfish@users.noreply.github.com>
2025-05-29 16:37:27 +0800

    Update utils.h

--- header/utils.h
+++ header/utils.h
@@ -8,5 +8,6 @@
 
 void print_results(const std::vector<Process>& processes, int context_switches, const std::string& algo_name);
 void print_gantt_chart(const std::map<int, int>& gantt_chart_data);
+void save_gantt_chart_data_to_csv(const std::map<int, int>& gantt_chart_data, const std::string& filename);
 
 #endif // UTILS_H
